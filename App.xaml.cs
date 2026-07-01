239503f91e5f8ce95068de9f06451e0ad3e76406
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-07-01 12:33:24 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PropertyApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LandingPage())
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ ViewModel/DetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyApp.ViewModel
+{
+    public class DetailsViewModel : BaseViewModel
+    {
+    }
+}
